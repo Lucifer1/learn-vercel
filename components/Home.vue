842601f63fd552cbb4f7d82629182a62c398f6a8
zhangjiamingbj <zhangjiamingbj@yuanfudao.com>
2023-04-17 18:39:23 +0800

    MOD: UI fix

--- components/Home.vue
+++ components/Home.vue
@@ -1,12 +1,14 @@
 <template>
-  <p>
-    <span id="a">欢</span>
-    <span id="e">乐</span>
-    <span id="i">的</span>
-    <span id="o">小</span>
-    <span id="u">盖</span>
-    <span id="p">盖</span>
-  </p>
+  <div class="wrapper">
+    <div class="content">
+      <span id="a">欢</span>
+      <span id="e">乐</span>
+      <span id="i">的</span>
+      <span id="o">小</span>
+      <span id="u">盖</span>
+      <span id="p">盖</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,48 +19,58 @@ export default {
 
 <style>
 body {
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #000000;
+  padding: 0;
+  margin: 0;
 }
 
-p {
+.wrapper {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: black;
+}
+
+.content {
+  width: 100vw;
   background: none;
   color: #999;
   font-family: 'Dancing Script', 'Warnes', cursive;
   font-size: 100px;
   letter-spacing: -26px;
   cursor: pointer;
+  text-align: center;
+  position: absolute;
+  top: 20%;
 }
 
-p span {
+.content span {
   animation: flicker 3s linear infinite;
   padding: 0 10px;
 }
 
-p #a {
+.content #a {
   animation-delay: .2s;
 }
 
-p #e {
+.content #e {
   animation-delay: .5s;
 }
 
-p #i {
+.content #i {
   animation-delay: .8s;
 }
 
-p #o {
+.content #o {
   animation-delay: 1.1s;
 }
 
-p #u {
+.content #u {
   animation-delay: 1.2s;
 }
 
-p #p {
+.content #p {
   animation-delay: 1.3s;
 }
 
