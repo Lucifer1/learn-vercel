9bb4566de42e1ae9b5fbc9eecf7d7d0b46be7b06
zhangjiamingbj <zhangjiamingbj@yuanfudao.com>
2023-04-17 15:10:34 +0800

    MOD: change home

---
+++ components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+    欢乐的小盖盖
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageHome'
+}
+</script>
+
+<style>
+.main {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 100px;
+}
+</style>
